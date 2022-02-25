d755aad6e8cbd29157746752b82c559e1a860b01
dblinkhorn <dblinkhorn@gmail.com>
2022-02-24 18:50:29 -0800

    tweaked video details styling and samples styling

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -9,9 +9,18 @@ const VideoDetails = (props) => {
     <div className="video-details__outer-container">
       <div className="video-details__inner-container">
         <div className="video-details__title">{video_title}</div>
-        <div className="video-details__video">Video URL: {video_url}</div>
+        <div className="video-details__video">
+          <video
+            className="video-mask"
+            playsInline
+            controls
+            style={{ opacity: 1, "width": "90%", "height": "90%" }}
+          >
+            <source src={video_url} type="video/mp4" />
+          </video>
+        </div>
         <div className="video-details__resources">
-          Video Resources: {video_resources}
+          <p>Additional Resources & Enrichment: {video_resources}</p>
         </div>
       </div>
     </div>
